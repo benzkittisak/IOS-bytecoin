13857c7084eaa1a2e6b839f729af48a54fc7c79e
benzkittisak <benzkittisak@stv.ac.th>
2022-06-26 08:18:29 +0700

    update extensions code

--- bytecoin/Controller/ViewController.swift
+++ bytecoin/Controller/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController ,  UIPickerViewDataSource , UIPickerViewDelegate , CoinManagerDelegate {
+class ViewController: UIViewController {
     
     
     @IBOutlet weak var currencyPicker: UIPickerView!
@@ -17,7 +17,7 @@ class ViewController: UIViewController ,  UIPickerViewDataSource , UIPickerViewD
     @IBOutlet weak var currencyLB: UILabel!
     
     var coinManager = CoinManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,29 +27,45 @@ class ViewController: UIViewController ,  UIPickerViewDataSource , UIPickerViewD
         coinManager.delegate = self
     }
     
+}
+
+
+//MARK: - UIPickerViewDataSource Section
+
+extension ViewController : UIPickerViewDataSource {
     
-//    จำนวน column ใน pickerview
+    //    จำนวน column ใน pickerview
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-//    จำนวนแถวใน pickerview
+    //    จำนวนแถวใน pickerview
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
+}
+
+//MARK: - UIPickerViewDelegate Section
+
+extension ViewController : UIPickerViewDelegate {
     
-//    กำหนดชื่อข้อข้อมูลที่อยู่ใน pickerview
+    //    กำหนดชื่อข้อข้อมูลที่อยู่ใน pickerview
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        row มันจะวนลูปชื่อของสกุลเงินตามจำนวนของข้อมูลที่เรากำหนดไว้ใน numberOfRowsInComponent น่ะนะ
+        //        row มันจะวนลูปชื่อของสกุลเงินตามจำนวนของข้อมูลที่เรากำหนดไว้ใน numberOfRowsInComponent น่ะนะ
         return coinManager.currencyArray[row]
     }
     
-//    เวลาเราเลื่อนแถบ picker ไปที่ข้อมูลตัวไหนให้มันดึงข้อมูลของตำแหน่งที่มันไปหยุดออกมา
+    
+    //    เวลาเราเลื่อนแถบ picker ไปที่ข้อมูลตัวไหนให้มันดึงข้อมูลของตำแหน่งที่มันไปหยุดออกมา
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let currentCurrency = coinManager.currencyArray[row]
         coinManager.getCoinPrice(for: currentCurrency)
     }
-    
+}
+
+//MARK: - CoinManagerDelegate Section
+
+extension ViewController : CoinManagerDelegate {
     func didUpdateRate(_ coinManager:CoinManager , rateData: CoinModel) {
         DispatchQueue.main.async {
             self.currencyLB.text = "\(rateData.rateToString) \(rateData.currency)"
@@ -59,6 +75,7 @@ class ViewController: UIViewController ,  UIPickerViewDataSource , UIPickerViewD
     func didUpdateRateFailure(_ error: Error) {
         print(error)
     }
-
 }
+
+
 
